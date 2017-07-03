762e5a9ff2ab3d5c09f8cf1917a8b84c6fbf9370
Finn Fitzsimons <fitzsimons.finn@gmail.com>
2017-07-03 12:31:33 +0300

    variable docs server port

--- docs/server.js
+++ docs/server.js
@@ -1,10 +1,10 @@
 var http = require('http'),
   url = require('url'),
   path = require('path'),
-  fs = require('fs')
-port = process.argv[2] || 4002;
+  fs = require('fs');
+port = process.env.PORT || process.argv[2] || 4002;
 
-http.createServer(function(request, response) {
+http.createServer(function (request, response) {
 
   var uri = url.parse(request.url).pathname
     , filename = path.join(process.cwd(), '../', uri);
@@ -13,9 +13,9 @@ http.createServer(function(request, response) {
 
   if (fs.statSync(filename).isDirectory()) filename += '/index.html';
 
-  fs.readFile(filename, 'binary', function(err, file) {
+  fs.readFile(filename, 'binary', function (err, file) {
     if (err) {
-      response.writeHead(500, {'Content-Type': 'text/plain'});
+      response.writeHead(500, { 'Content-Type': 'text/plain' });
       response.write(err + '\n');
       response.end();
       return;
